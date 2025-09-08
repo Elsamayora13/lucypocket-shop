fc8d6148b9a954d7b72c474acacea3c8cbf65d76
Elsamayora13 <elsamayora132@gmail.com>
2025-09-08 20:48:24 +0700

    Add url PWS

--- lucylocket_shop/settings.py
+++ lucylocket_shop/settings.py
@@ -31,7 +31,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 DEBUG = True
 
 ...
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "elsa-mayora-lucylocketshop.pbp.cs.ui.ac.id"]
 ...
 
 
